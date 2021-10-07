d5ab74192e03b9a697850649c0bddde52dabeba9
SargsyanSasun <sasun.sargsyan01@gmail.com>
2021-10-08 01:28:52 +0400

    first commit

---
+++ src/homework/FigurePainter.java
@@ -0,0 +1,143 @@
+package homework;
+
+public class FigurePainter {
+
+
+    public static void main(String[] args) {
+
+        //w              Ss
+
+      /*    *
+            * *
+            * * *
+            * * * *
+
+        for (int f = 0; f <4 ; f++) {
+            for (int r = 0; r <f+1; r++)
+                System.out.print("* ");
+            System.out.println();}
+
+            * * * *
+            * * *
+            * *
+            *
+           for (int j = 0; j <4; j++)
+                System.out.print("* ");
+            System.out.println();
+            for (int j = 0; j <3; j++)
+                System.out.print("* ");
+            System.out.println();
+            for (int j=0; j<2; j++)
+                System.out.print("* ");
+            System.out.println();
+            for (int j = 0; j <1; j++)
+                System.out.print("* ");
+            System.out.println();   */
+
+         /*            *
+                      * *
+                     * * *
+                    * * * *
+
+        for (int j = 0; j <4; j++)
+            System.out.print(" ");
+        for (int r = 0; r <1; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j = 0; j <3; j++)
+            System.out.print(" ");
+        for (int r = 0; r <2; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j=0; j<2; j++)
+            System.out.print(" ");
+        for (int r = 0; r <3; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j = 0; j <1; j++)
+            System.out.print(" ");
+        for (int r = 0; r <4; r++)
+            System.out.print("* ");
+        System.out.println();     */
+
+
+        /*           *
+                   * *
+                 * * *
+               * * * *
+        for (int j = 0; j <4; j++)
+            System.out.print(" ");
+            for (int r = 0; r <1; r++)
+                System.out.print("     *");
+        System.out.println();
+        for (int j = 0; j <3; j++)
+            System.out.print("  ");
+        for (int r = 0; r <2; r++)
+            System.out.print(" *");
+        System.out.println();
+        for (int j=0; j<2; j++)
+            System.out.print("  ");
+        for (int r = 0; r <3; r++)
+            System.out.print(" *");
+        System.out.println();
+        for (int j = 0; j <1; j++)
+            System.out.print("  ");
+        for (int r = 0; r <4; r++)
+            System.out.print(" *");
+        System.out.println();  */
+
+      /*          *
+                 * *
+                * * *
+               * * * *
+                * * *
+                 * *
+                  *
+
+        for (int j = 0; j <4; j++)
+            System.out.print(" ");
+        for (int r = 0; r <1; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j = 0; j <3; j++)
+            System.out.print(" ");
+        for (int r = 0; r <2; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j=0; j<2; j++)
+            System.out.print(" ");
+        for (int r = 0; r <3; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j = 0; j <1; j++)
+            System.out.print(" ");
+        for (int r = 0; r <4; r++)
+            System.out.print("* ");
+        System.out.println();
+
+        for (int j = 0; j <2; j++)
+            System.out.print(" ");
+        for (int r = 0; r <3; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j=0; j<3; j++)
+            System.out.print(" ");
+        for (int r = 0; r <2; r++)
+            System.out.print("* ");
+        System.out.println();
+        for (int j = 0; j <4; j++)
+            System.out.print(" ");
+        for (int r = 0; r <1; r++)
+            System.out.print("* ");
+        System.out.println();  */
+
+
+
+
+
+
+
+
+    }
+
+}
